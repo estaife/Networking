4eccf4569485ad1e771b1f6b5c4c5f2e440be84e
Estaife Lima <estaifeh@gmail.com>
2022-12-09 23:49:37 -0400

    Implementation of HTTPRequestDispatcher

---
+++ Sources/Networking/HTTPRequestDispatcher.swift
@@ -0,0 +1,82 @@
+//
+//  HTTPRequestDispatcher.swift
+//  
+//
+//  Created by estaife on 09/12/22.
+//
+
+import Foundation
+
+public class HTTPRequestDispatcher: HTTPRequestDispatcherProtocol {
+    public let session: URLSessionProtocol
+    public let builder: URLRequestBuilderProtocol
+    
+    public required init(session: URLSessionProtocol, builder: URLRequestBuilderProtocol) {
+        self.session = session
+        self.builder = builder
+    }
+    
+    public convenience init() {
+        self.init(session: URLSession.shared, builder: URLRequestBuilder())
+    }
+   
+    public func execute<ResponseType: Codable>(_ request: HTTPRequestProtocol, type: ResponseType.Type, completion: @escaping (Result<ResponseType, HTTPRequestError>) -> Void) {
+        
+        do {
+            let urlRequest = try builder.builder(with: request)
+            session.dataTask(with: urlRequest) { (data, response, error) in
+                if let error {
+                    switch error {
+                    case let urlError as URLError:
+                        if urlError.networkUnavailableReason != nil {
+                            completion(.failure(.networkUnavailable))
+                        }
+                        completion(.failure(.urlError(urlError)))
+                        
+                    case let requestError as HTTPRequestError:
+                        completion(.failure(requestError))
+                        
+                    default:
+                        completion(.failure(.request(error)))
+                    }
+                }
+                
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    completion(.failure(.invalidHTTPResponse))
+                    return
+                }
+                
+                guard let data else {
+                    completion(.failure(.unknown))
+                    return
+                }
+                
+                guard 200...299 ~= httpResponse.statusCode else {
+                    completion(.failure(.serializedError(data: data, statusCode: httpResponse.statusCode)))
+                    return
+                }
+        
+                do {
+                    let decoder = JSONDecoder()
+                    let responseType = try decoder.decode(type.self, from: data)
+                    completion(.success(responseType))
+                } catch let error as DecodingError {
+                    completion(.failure(.jsonParse(error)))
+                } catch {
+                    completion(.failure(.unknown))
+                }
+                
+                completion(.failure(.unknown))
+            }
+        } catch {
+            completion(.failure(.request(error)))
+        }
+    }
+    
+    public func execute(_ url: URL, completion: @escaping (Result<Data, HTTPRequestError>) -> Void) {
+        session.dataTask(with: url) { data, _, error in
+            if let error { completion(.failure(.request(error))) }
+            if let data { completion(.success(data)) }
+        }
+    }
+}
