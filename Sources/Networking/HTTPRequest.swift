5f6285d2f7bd41eaa42da05862694c5e4e1c3301
Estaife Lima <estaifeh@gmail.com>
2022-12-09 23:39:22 -0400

    Make HTTPRequest to define an request

---
+++ Sources/Networking/HTTPRequest.swift
@@ -0,0 +1,34 @@
+//
+//  HTTPRequest.swift
+//  
+//
+//  Created by estaife on 09/12/22.
+//
+
+import Foundation
+
+public protocol HTTPRequestProtocol {
+    var url: String { get }
+    var method: HTTPMethod { get }
+    var headers: [String: String]? { get }
+    var parameters: HTTPRequestParameters? { get }
+}
+
+public enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+}
+
+public enum HTTPRequestParameters {
+    case queryItems([String: String])
+    case json(model: Encodable)
+    case body([String: Any]?)
+    case plain
+}
+
+
+public extension Encodable {
+    func convertToData() throws -> Data {
+        return try JSONEncoder().encode(self)
+    }
+}
