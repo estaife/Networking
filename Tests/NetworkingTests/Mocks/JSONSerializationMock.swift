a206cfbca657793c29e6db16de08556ff5de1429
Estaife Lima <estaifeh@gmail.com>
2022-12-11 20:33:26 -0400

    Added func isValidJSONObject

--- Tests/NetworkingTests/Mocks/JSONSerializationMock.swift
+++ Tests/NetworkingTests/Mocks/JSONSerializationMock.swift
@@ -9,11 +9,19 @@ import Foundation
 
 final class JSONSerializationMock: JSONSerialization {
     static var errorStub: Error?
+    static var isValidJSONObjectStub = true
     
     override class func data(withJSONObject obj: Any, options opt: JSONSerialization.WritingOptions = []) throws -> Data {
         if let errorStub {
             throw errorStub
         }
         return try super.data(withJSONObject: obj, options: opt)
+    }
+    
+    override class func isValidJSONObject(_ obj: Any) -> Bool {
+        if isValidJSONObjectStub {
+            return super.isValidJSONObject(obj)
+        }
+        return isValidJSONObjectStub
     }
 }
