63328598938c0eb879be976a1ce1ce4c5a21928b
Estaife Lima <estaifeh@gmail.com>
2022-12-11 20:39:11 -0400

    Make some refactors into URLRequestBuilderTests and added new units tests

--- Tests/NetworkingTests/Tests/URLRequestBuilderTests.swift
+++ Tests/NetworkingTests/Tests/URLRequestBuilderTests.swift
@@ -25,16 +25,16 @@ final class URLRequestBuilderTests: XCTestCase {
         encoderMock = nil
     }
 
-    func testBuilderWithURLInvalid() throws {
-        let requestSpy = HTTPRequestSpy(url: invalidURLString)
+    func testBuildWithURLInvalid() throws {
+        let requestSpy = HTTPRequestSpy(url: Seeds.invalidURLString)
         
         XCTAssertThrowsError(try sut.build(with: requestSpy)) { error in
-            XCTAssertEqual(error as! HTTPRequestError, HTTPRequestError.urlError(URLError(URLError.Code.badURL)))
+            XCTAssertEqual(error as! HTTPRequestError, HTTPRequestError.parameterEncodingFailed(reason: .missingURL))
         }
     }
 
-    func testBuilderWithURLValid() throws {
-        let requestSpy = HTTPRequestSpy(url: validURLString)
+    func testBuildWithURLValid() throws {
+        let requestSpy = HTTPRequestSpy(url: Seeds.urlString)
     
         let urlRequest = try sut.build(with: requestSpy)
         
@@ -44,67 +44,76 @@ final class URLRequestBuilderTests: XCTestCase {
         XCTAssertNil(urlRequest.httpBody)
     }
     
-    func testBuilderWithHeaders() throws {
+    func testBuildWithHeaders() throws {
         let headers: [String : String] = ["keyOne": "valueOne", "keyTwo": "valueTwo"]
-        let requestSpy = HTTPRequestSpy(url: validURLString, headers: headers)
+        let requestSpy = HTTPRequestSpy(url: Seeds.urlString, headers: headers)
         
         let urlRequest = try sut.build(with: requestSpy)
         
         XCTAssertEqual(urlRequest.allHTTPHeaderFields, requestSpy.headers)
     }
     
-    func testBuilderWithQueryItemsParameters() throws {
+    func testBuildWithQueryItemsParameters() throws {
         let expectedQuery = "keyOne=valueOne"
         let items: [String : String] = ["keyOne": "valueOne"]
-        let requestSpy = HTTPRequestSpy(url: validURLString, method: .post, headers: nil, parameters: .queryItems(items))
+        let requestSpy = HTTPRequestSpy(url: Seeds.urlString, parameters: .queryItems(items))
     
         let urlRequest = try sut.build(with: requestSpy)
         
         XCTAssertEqual(urlRequest.url?.query, expectedQuery)
     }
     
-    func testBuilderWithJSONParameters() throws {
-        let testModel = TestModel(test: "test")
-        let requestSpy = HTTPRequestSpy(url: validURLString, method: .post, parameters: .json(model: testModel))
+    func testBuildWithJSONParameters() throws {
+        let requestSpy = HTTPRequestSpy(url: Seeds.urlString, method: .post, parameters: .json(model: Seeds.testModel))
         
         let urlRequest = try sut.build(with: requestSpy)
         
-        XCTAssertEqual(urlRequest.httpBody, try testModel.convertToData())
+        XCTAssertEqual(urlRequest.httpBody, try Seeds.testModel.convertToData())
+        XCTAssertEqual(urlRequest.httpMethod, "POST")
+        XCTAssertEqual(urlRequest.allHTTPHeaderFields?["Content-Type"], "application/json")
     }
     
-    func testBuilderWithJSONParametersParseError() throws {
+    func testBuildWithJSONParametersEncodingFailed() throws {
         let testModel = TestModel(test: "test")
-        let requestSpy = HTTPRequestSpy(url: validURLString, method: .post, parameters: .json(model: testModel))
-        let errorExpected = NSError(domain: "Json Serializer Error", code: -1)
-        encoderMock.errorStub = errorExpected
+        let requestSpy = HTTPRequestSpy(url: Seeds.urlString, method: .post, parameters: .json(model: testModel))
+        encoderMock.errorStub = Seeds.error
         
         let sut = URLRequestBuilder(jsonEncoder: encoderMock)
         
         XCTAssertThrowsError(try sut.build(with: requestSpy)) { error in
-            XCTAssertEqual(error as! HTTPRequestError, HTTPRequestError.requestSerialization(errorExpected))
+            XCTAssertEqual(error as! HTTPRequestError, HTTPRequestError.parameterEncodingFailed(reason: .customEncodingFailed(error: Seeds.error)))
         }
     }
     
-    func testBuilderWithBodyParameters() throws {
-        let url = "http://example.com"
-        let dictionaryBody = ["fieldOne": "valueOne", "fieldTwo" : ["subFieldTwo" : "subFieldTwo"]] as [String : Any]
-        let requestSpy = HTTPRequestSpy(url: url, method: .post, parameters: .body(dictionaryBody))
+    func testBuildWithBodyParameters() throws {
+        let requestSpy = HTTPRequestSpy(url: Seeds.urlString, method: .post, parameters: .body(Seeds.dictionaryBody))
 
         let urlRequest = try sut.build(with: requestSpy)
         
         XCTAssertNotNil(urlRequest.httpBody)
+        XCTAssertEqual(urlRequest.httpMethod, "POST")
+        XCTAssertEqual(urlRequest.allHTTPHeaderFields?["Content-Type"], "application/json")
     }
     
-    func testBuilderWithBodyParametersParseError() throws {
-        let dictionaryBody = ["fieldOne": "valueOne", "fieldTwo" : ["subFieldTwo" : "subFieldTwo"]] as [String : Any]
-        let requestSpy = HTTPRequestSpy(url: validURLString, method: .post, parameters: .body(dictionaryBody))
-        let expectedError = NSError(domain: "Json Serializer Error", code: -1)
+    func testBuildWithBodyInvalidJSON() throws {
+        let requestSpy = HTTPRequestSpy(url: Seeds.urlString, method: .post, parameters: .body(Seeds.dictionaryBody))
+        let sut = URLRequestBuilder(jsonSerializer: JSONSerializationMock.self)
         
-        JSONSerializationMock.errorStub = expectedError
+        JSONSerializationMock.isValidJSONObjectStub = false
+        
+        XCTAssertThrowsError(try sut.build(with: requestSpy)) { error in
+            XCTAssertEqual(error as! HTTPRequestError, HTTPRequestError.parameterEncodingFailed(reason: .invalidJSONObject))
+        }
+    }
+    
+    func testBuilderWithBodyParametersEncodingFailed() throws {
+        let requestSpy = HTTPRequestSpy(url: Seeds.urlString, method: .post, parameters: .body(Seeds.dictionaryBody))
+        
+        JSONSerializationMock.errorStub = Seeds.error
         let sut = URLRequestBuilder(jsonSerializer: JSONSerializationMock.self)
         
         XCTAssertThrowsError(try sut.build(with: requestSpy)) { error in
-            XCTAssertEqual(error as! HTTPRequestError, HTTPRequestError.requestSerialization(expectedError))
+            XCTAssertEqual(error as! HTTPRequestError, HTTPRequestError.parameterEncodingFailed(reason: .jsonEncodingFailed(error: Seeds.error)))
         }
     }
 }
