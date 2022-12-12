01488050365699a125567df0336cec3a312f68df
Estaife Lima <estaifeh@gmail.com>
2022-12-11 20:36:37 -0400

    Conforms to HTTPRequestProtocol

--- Tests/NetworkingTests/Spys/HTTPRequestSpy.swift
+++ Tests/NetworkingTests/Spys/HTTPRequestSpy.swift
@@ -10,10 +10,10 @@ import Networking
 struct HTTPRequestSpy: HTTPRequestProtocol {
     var url: String
     var method: HTTPMethod
-    var headers: [String : String]?
+    var headers: [String : String]
     var parameters: HTTPRequestParameters
     
-    init(url: String, method: HTTPMethod = .get, headers: [String : String]? = nil, parameters: HTTPRequestParameters = .plain) {
+    init(url: String, method: HTTPMethod = .get, headers: [String : String] = [:], parameters: HTTPRequestParameters = .plain) {
         self.url = url
         self.method = method
         self.headers = headers
